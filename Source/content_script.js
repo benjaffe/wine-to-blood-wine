265d1f93097fe96efc780e35be8a4ced3065ef8e
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-16 08:43:15 -0700

    perf gain
    module pattern for holding the list

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,29 +1,4 @@
-walk(document.body);
-
-function walk(node) { // Function taken from http://is.gd/mwZp7E
-
-	var child, next;
-	switch ( node.nodeType ) {
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) {
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode) {
-	var v = textNode.nodeValue;
-	var	i, len, re;
+window.WineToBloodwine = (function(){
 	var arr = [
 	    ["human"   , "hewmon"],
 	    ["Human"   , "Hewmon"],
@@ -40,17 +15,47 @@ function handleText(textNode) {
 	    ["Cisco"   , "Sisko"]
 	];
 
+	// make RegExps
 	for (i = 0, len = arr.length; i < len; i++) {
 		arr[i][0] = new RegExp( arr[i][0] );
 	}
 
-	for (i = 0, len = arr.length; i < len; i++) {
-		re = arr[i][0];
-		if ( re.test(v) ) {
-			v = v.replace(re, arr[i][1]);
+	function walk(node) { // Function taken from http://is.gd/mwZp7E
+		var child, next;
+		switch ( node.nodeType ) {
+			case 1:  // Element
+			case 9:  // Document
+			case 11: // Document fragment
+				child = node.firstChild;
+				while ( child ) {
+					next = child.nextSibling;
+					walk(child);
+					child = next;
+				}
+				break;
+
+			case 3: // Text node
+				handleText(node);
+				break;
 		}
 	}
 
-	textNode.nodeValue = v;
-}
+	function handleText(textNode) {
+		var v = textNode.nodeValue;
+		var	i, len, re;
 
+		for (i = 0, len = arr.length; i < len; i++) {
+			re = arr[i][0];
+			if ( re.test(v) ) {
+				v = v.replace(re, arr[i][1]);
+			}
+		}
+
+		textNode.nodeValue = v;
+	}
+
+	walk(document.body);
+
+})();
+
+Bloodwine = Bloodwine || WineToBloodwine;
