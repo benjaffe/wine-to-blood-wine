e9cb1b7ad4d8eb42e607e6a9cad8e292b9284595
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-17 06:36:35 -0700

    got Jasmine tests working (thanks to @ecmendenhall)
    https://gist.github.com/ecmendenhall/3740896

---
+++ Source/tests/loadjasmine.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("loadjasmine").addEventListener('click', loadJasmine);
+});
+
+function loadJasmine() {
+  console.log("Loading Jasmine...");
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+
+  var htmlReporter = new jasmine.HtmlReporter();
+
+  jasmineEnv.addReporter(htmlReporter);
+
+  jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+  };
+
+  function execJasmine() {
+    jasmineEnv.execute();
+  }
+  function runTests() {
+    alert('hi');
+    execJasmine();
+  }
+  document.getElementById("runtests").addEventListener('click', runTests);
+}
