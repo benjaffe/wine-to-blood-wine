37a7e322c1eb3278f9661d48fc65a8f23cc4013b
Ben Jaffe <jaffe.ben@gmail.com>
2014-05-17 17:01:15 -0700

    updated tests

--- Source/tests/spec/replacements.js
+++ Source/tests/spec/replacements.js
@@ -1,30 +1,39 @@
+function getValueOfElemById(id) {
+    var pElem = document.getElementById(id);
+    var originals = pElem.querySelectorAll('.wtbw-original');
+    var str = '';
+    for (var i = 0; i < originals.length; i++) {
+        originals[i].parentNode.removeChild(originals[i]);
+    }
+    return pElem.textContent;
+}
 describe("Replacement Tests", function() {
     it("expects human to become hewmon", function() {
-        expect( window.Wtbw.makeReplacements('Human, human, Humans, humans, HUman') )
-            .toBe('Hewmon, hewmon, Hewmons, hewmons, HUman');
+        expect( getValueOfElemById('human') )
+            .toBe('Hewmon, hewmon, Hewmons, hewmons, hewmon');
     });
     it("expects wine to become bloodwine", function() {
-        expect( window.Wtbw.makeReplacements('Human, human, Humans, humans, HUman') )
-            .toBe('Hewmon, hewmon, Hewmons, hewmons, HUman');
+        expect( getValueOfElemById('wine') )
+            .toBe('bloodwine, Bloodwine');
     });
     it("expects beer to become Romulan ale", function() {
-        expect( window.Wtbw.makeReplacements('beer, Beer') )
+        expect( getValueOfElemById('beer') )
             .toBe('Romulan ale, Romulan Ale');
     });
     it("expects coffee to become Raktajino", function() {
-        expect( window.Wtbw.makeReplacements('coffee, Coffee') )
+        expect( getValueOfElemById('coffee') )
             .toBe('raktajino, Raktajino');
     });
     it("expects tea to become Tarkalean tea", function() {
-        expect( window.Wtbw.makeReplacements('tea, Tea, Earl Grey Tea, Earl Grey tea, Earl Grey') )
-            .toBe('Tarkalean tea, Tarkalean Tea, Tea (Earl Grey, hot), tea (Earl Grey, hot), tea (Earl Grey, hot)');
+        expect( getValueOfElemById('tea') )
+            .toBe('Tarkalean tea, Tarkalean Tea, Tea (Earl Grey, hot), tea (Earl Grey, hot), Earl Grey');
     });
     it("expects brandy to become Saurian brandy", function() {
-        expect( window.Wtbw.makeReplacements('brandy, Brandy') )
+        expect( getValueOfElemById('brandy') )
             .toBe('Saurian brandy, Saurian Brandy');
     });
     it("expects Cisco to become Sisko", function() {
-        expect( window.Wtbw.makeReplacements('Cisco Systems') )
-            .toBe('Sisko Systems');
+        expect( getValueOfElemById('cisco') )
+            .toBe('Sisko, Sisko');
     });
 });
